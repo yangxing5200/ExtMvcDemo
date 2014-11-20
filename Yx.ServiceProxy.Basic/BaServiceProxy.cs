a92585d84f1486c338ed3a0fa30ffc2586b5fe4d
yangtingxing@outlook.com <yangtingxing@outlook.com>
2014-11-20 11:14:10 +0800

    添加WCF 层

---
+++ Yx.ServiceProxy.Basic/BaServiceProxy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yx.ServiceProxy.Basic.BasicService;
+
+namespace Yx.ServiceProxy.Basic
+{
+    public class BaServiceProxy:IBasicService
+    {
+        public string GetData(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+ 
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
